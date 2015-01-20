698c986a83dd3a3da0eeb3a3f4dd88a8d891a557
Andrey Rykhalsky <AndreyRykhalsky@coherentsolutions.com>
2015-01-20 20:08:25 +0300

    Initial commit

---
+++ src/main/java/com/sprsec/model/User.java
@@ -0,0 +1,58 @@
+package com.sprsec.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String login;
+	
+	private String password;
+	
+	private Integer role;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getRole() {
+		return role;
+	}
+
+	public void setRole(Integer role) {
+		this.role = role;
+	}	
+
+}
